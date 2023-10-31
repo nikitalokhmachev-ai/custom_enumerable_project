a146875d7a09cecb53be8cfefb264698d9ec8707
nikitalokhmachev-ai <lokhmachevnikita@gmail.com>
2023-10-31 11:35:50 -0400

    implement custom enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,75 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    for i in 0...length
+      yield self[i], i
+    end
+    self
+  end
+
+  def my_select
+    results = []
+    for el in self
+      results.push(el) if yield el
+    end
+    results
+  end
+
+  def my_all?
+    results = []
+    for el in self
+      results.push(el) if yield el
+    end
+    results.length == length
+  end
+
+  def my_any?
+    results = []
+    for el in self
+      results.push(el) if yield el
+    end
+    !results.empty?
+  end
+
+  def my_none?
+    results = []
+    for el in self
+      results.push(el) if yield el
+    end
+    results.empty?
+  end
+
+  def my_count
+    if block_given?
+      results = []
+      for el in self
+        results.push(el) if yield el
+      end
+      results.length
+    else
+      length
+    end
+  end
+
+  def my_map
+    result = []
+    for el in self
+      result.push(yield el)
+    end
+    result
+  end
+
+  def my_inject(initial_value)
+    first_iter = true
+    for val in self
+      if first_iter
+        result = yield initial_value, val
+        first_iter = false
+      else
+        result = yield result, val
+      end
+    end
+    result
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +77,9 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for el in self
+      yield el
+    end
+  end
 end
